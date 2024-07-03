6c048f3dca230508a3a079a181be162f7cb20c93
Luke Hefson <lukehefson@gmail.com>
2024-07-03 16:05:20 -0400

    Initial Commit

---
+++ SpellCheckService/SpellCheckServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellCheckServiceApp.swift
+//  SpellCheckService
+//
+//  Created by Luke Hefson on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpellCheckServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
